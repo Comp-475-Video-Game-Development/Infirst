a594c305126c71ec84ef507a7ff1d99ba013c053
nburner <nburner@harding.edu>
2018-04-21 23:36:09 -0500

    Key Interaction
    - Added key prefabs
    - Player can pick up keys
    - Text appears in bottom middle to show when you are close to a key

---
+++ Assets/Scripts/KeyInteraction.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyInteraction : MonoBehaviour
+{
+    public GameObject KeyText;
+    public GameObject KeyPrefab;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            KeyText.SetActive(false);
+            Destroy(KeyPrefab);
+        }
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            KeyText.SetActive(true);
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            KeyText.SetActive(false);
+        }
+    }
+}
