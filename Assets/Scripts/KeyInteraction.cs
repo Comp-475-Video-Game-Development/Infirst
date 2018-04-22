d49a09f2a6964ba10ce82d6f55bb8641101cfabc
nburner <nburner@harding.edu>
2018-04-22 01:17:19 -0500

    Door Interaction
    - KeyInteraction and DoorInteraction scripts comunicate with each other.  If a key corresponds to a certain door and that key has been picked up, then the door will open when player presses "F"
    - Player can now leave spawn

--- Assets/Scripts/KeyInteraction.cs
+++ Assets/Scripts/KeyInteraction.cs
@@ -1,17 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class KeyInteraction : MonoBehaviour
 {
+    public Text DoorText;
     public GameObject KeyText;
     public GameObject KeyPrefab;
+    bool inProximity = false;
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.F))
+        if (Input.GetKeyDown(KeyCode.F) && inProximity)
         {
             KeyText.SetActive(false);
+            DoorText.text = "Press F to open";
             Destroy(KeyPrefab);
         }
     }
@@ -20,6 +24,7 @@ public class KeyInteraction : MonoBehaviour
     {
         if (other.gameObject.tag == "Player")
         {
+            inProximity = true;
             KeyText.SetActive(true);
         }
     }
@@ -28,6 +33,7 @@ public class KeyInteraction : MonoBehaviour
     {
         if (other.gameObject.tag == "Player")
         {
+            inProximity = false;
             KeyText.SetActive(false);
         }
     }
