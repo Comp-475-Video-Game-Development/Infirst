14a41fe37462b4aff61364fdf8284220c7656880
nburner <nburner@harding.edu>
2018-04-22 14:09:25 -0500

    Pause Menu and Music
    - Can now pause the game
    - Ambient sound has been added

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject PausePanel;
+    public GameObject PlayerController;
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!PausePanel.activeInHierarchy)
+            {
+                PausePanel.SetActive(true);
+                PlayerController.GetComponent<FirstPersonController>().enabled = false;
+                Time.timeScale = 0;
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+            }
+            else
+            {
+                PausePanel.SetActive(false);
+                PlayerController.GetComponent<FirstPersonController>().enabled = true;
+                Time.timeScale = 1;
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = false;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/VolumeController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeController : MonoBehaviour
+{
+    public Slider slider;
+
+    public void ChangeVolume()
+    {
+        AudioListener.volume = slider.value;
+    }
+}
