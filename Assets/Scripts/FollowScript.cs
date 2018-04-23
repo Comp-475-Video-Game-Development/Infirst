c231cb5133157fd38c09376ad70ab2ee21c77c2c
Nathan Burner <nburner@harding.edu>
2018-04-23 15:00:21 -0500

    Major improvments to zombie

--- Assets/Scripts/FollowScript.cs
+++ Assets/Scripts/FollowScript.cs
@@ -6,25 +6,25 @@ public class FollowScript : MonoBehaviour {
 
     private Transform enemyTransform;
     public GameObject player;
-  
+    private Animator anim;
     public int maxDistance;
     public float minDistance;
     public int moveSpeed;
     public int rotationSpeed;
     public static bool follow = false;
-    
+    private bool isWalking = false;
+    private bool isMoving = false;
+    private bool isAttacking = false;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
 
     private void Awake()
     {
         enemyTransform = transform;
     }
-
-    private void Start()
-    {
-        
-        GetComponent<Animation>().Play();
-    }
-
 
     private void Update()
     {
@@ -45,7 +45,45 @@ public class FollowScript : MonoBehaviour {
                 //Move tward player
                 //transform.LookAt(player.transform);
                 transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed);
+
+                isMoving = true;
+
+                if (!isWalking)
+                {
+                    anim.ResetTrigger("Attack");
+                    anim.SetTrigger("Walk");
+                    isWalking = true;
+                    isAttacking = false;
+                }
             }
+            else if (Vector3.Distance(player.transform.position, enemyTransform.position) <= minDistance)
+            {
+
+                if (!isAttacking)
+                {
+                    isAttacking = true;
+
+                    anim.SetTrigger("Attack");
+                    anim.ResetTrigger("Walk");
+                    isMoving = false;
+                    isWalking = false;
+                }
+
+            }
+            else
+            {
+                isMoving = false;
+
+                if(isWalking)
+                {
+                    isWalking = false;
+                    isAttacking = false;
+                    anim.SetTrigger("Idle");
+                }
+
+            }
+
+            
         }
     }
 }
