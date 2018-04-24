540ea029677d98c6b7cd1415f6d59dbcf471eab6
nburner <nburner@harding.edu>
2018-04-24 01:07:17 -0500

    Potentially Added Volume Prefs

--- Assets/Scripts/VolumeController.cs
+++ Assets/Scripts/VolumeController.cs
@@ -7,8 +7,26 @@ public class VolumeController : MonoBehaviour
 {
     public Slider slider;
 
+    private void Start()
+    {
+        if (PlayerPrefs.HasKey("Volume"))
+        {
+            slider.value = PlayerPrefs.GetFloat("Volume");
+            AudioListener.volume = slider.value;
+        }
+        else
+        {
+            PlayerPrefs.SetFloat("Volume", 0.5f);
+            PlayerPrefs.Save();
+            slider.value = PlayerPrefs.GetFloat("Volume");
+            AudioListener.volume = slider.value;
+        }
+    }
+
     public void ChangeVolume()
     {
         AudioListener.volume = slider.value;
+        PlayerPrefs.SetFloat("Volume", AudioListener.volume);
+        PlayerPrefs.Save();
     }
 }
