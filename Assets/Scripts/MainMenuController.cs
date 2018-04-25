aae80f79c431e267ea785f8a1ffa350505d87b5e
nburner <nburner@harding.edu>
2018-04-24 21:26:32 -0500

    Perfected Main Menu

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public GameObject ConfirmQuitPanel;
+
+    public void LoadAsylum()
+    {
+        Debug.Log("Loading Asylum");
+        SceneManager.LoadScene("Asylum");
+    }
+
+    public void ExitToDesktop()
+    {
+        ConfirmQuitPanel.SetActive(true);
+    }
+
+    public void ConfirmQuit()
+    {
+        Application.Quit();
+    }
+
+    public void DenyQuit()
+    {
+        ConfirmQuitPanel.SetActive(false);
+    }
+}
